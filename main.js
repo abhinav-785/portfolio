edfee048c722560a5ce25d41c558400d87c2d779
Abhinav Choudhary <abhifrbsp@gmail.com>
2025-02-01 00:43:39 +0530

    changed profile

--- main.js
+++ main.js
@@ -21,3 +21,28 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true,
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".navbar a");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= sectionTop - 50) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+
+            if (link.getAttribute("href").includes(current)) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
