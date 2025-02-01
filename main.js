8e39e3499461e4fcf828d6b857be2ed1aa2b0983
Abhinav Choudhary <abhifrbsp@gmail.com>
2025-02-02 02:05:30 +0530

    check

--- main.js
+++ main.js
@@ -5,57 +5,39 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
 menuIcon.classList.toggle('fa-xmark');
-navbar. classList. toggle('active');
+navbar.classList.toggle('active');
 }
+
+
 
 /*=================== scroll section active link ========================*/
 // let sections = document.querySelectorAll('section'); 
 // let navLinks = document.querySelectorAll('header nav a');
-// window.onscrol1 = () => {
-// sections.forEach(sec => {
-//      let top = window.scrollY;
-// let offset = sec.offsetTop - 150; 
-// let height = sec.offsetHeight; 
-// let id = sec.getAttribute('id');
-// if (top >= offset && top < offset + height) {
-//  navLinks.forEach.apply(links => {
-// links.classList.remove('active');
-// document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-// });
-// });
-// });
-// }
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150; 
+//         let height = sec.offsetHeight; 
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(link => {
+//                 link.classList.remove('active');
+//             });
+//             document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+//         }
+//     });
 
 
+// /*============================  sticky navbar   ==================================*/
 
-/*=================== scroll section active link ========================*/
-let sections = document.querySelectorAll('section'); 
-let navLinks = document.querySelectorAll('header nav a');
+// let header = document.querySelector('header');
+// header.classList.toggle('sticky',window.scrollY>100);
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150; 
-        let height = sec.offsetHeight; 
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-        }
-    });
-
-
-/*============================  sticky navbar   ==================================*/
-
-let header = document.querySelector('header');
-header.classList.toggle('sticky',window.scrollY>100);
-
-menuIcon.classList.remove('fa-xmark');
-navbar.classList.remove('active');
-};
+// menuIcon.classList.remove('fa-xmark');
+// navbar.classList.remove('active');
+// };
 
 /*============================  scroll reveal   ==================================*/
 ScrollReveal({
